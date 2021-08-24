1ca5e52369aa2357edcd94946d2139a77439985d
Auros Nexus <aurosnex@gmail.com>
2021-08-24 16:45:01 -0400

    Created Suite

---
+++ BeatSaberMarkupLanguage.Suite/Bootstrap.cs
@@ -0,0 +1,30 @@
+﻿using SiraUtil.Attributes;
+using SiraUtil.Logging;
+using SiraUtil.Zenject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeatSaberMarkupLanguage.Suite
+{
+    [Bind(Location.Menu)]
+    public class Bootstrap : IAsyncInitializable
+    {
+        private readonly SiraLog _siraLog;
+        private readonly MainFlowCoordinator _mainFlowCoordiantor;
+
+        public Bootstrap(SiraLog siraLog, MainFlowCoordinator mainFlowCoordinator)
+        {
+            _siraLog = siraLog;
+            _mainFlowCoordiantor = mainFlowCoordinator;
+        }
+
+        public async Task InitializeAsync(CancellationToken token)
+        {
+            await Task.Delay(1000);
+            _siraLog.Info("Presenting UI");
+        }
+    }
+}
---
+++ BeatSaberMarkupLanguage.Suite/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using IPA;
+using SiraUtil.Attributes;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BeatSaberMarkupLanguage.Suite
+{
+    [Plugin(RuntimeOptions.DynamicInit), Slog]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger logger, Zenjector zenjector)
+        {
+            zenjector.UseLogger(logger);
+            zenjector.UseAutoBinder();
+        }
+
+        [OnEnable, OnDisable]
+        public void OnState() => _ = true;
+    }
+}
